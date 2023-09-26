10239fc3c5e47cdbdbd0412e79fba36b2a415742
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2023-09-26 17:58:47 +0000

    test 11 fix 2

--- universal.js
+++ universal.js
@@ -54,31 +54,33 @@ function createRandomCard() {
     } else {
         let name = 'wacky name';
         let hp = Math.floor(Math.random() * (100 - 0 + 1) ) + 0;
+        let atk, def, defcha, critcha;
+
         if (rarity == 'common') {
-        let atk = Math.floor(Math.random() * (3 - 1 + 1) ) + 1;
-        let def = Math.floor(Math.random() * (3 - 1 + 1) ) + 1;
-        let defcha = Math.floor(Math.random() * (3 - 0 + 1) ) + 0;
-        let critcha = Math.floor(Math.random() * (3 - 0 + 1) ) + 0;
+        atk = Math.floor(Math.random() * (3 - 1 + 1) ) + 1;
+        def = Math.floor(Math.random() * (3 - 1 + 1) ) + 1;
+        defcha = Math.floor(Math.random() * (3 - 0 + 1) ) + 0;
+        critcha = Math.floor(Math.random() * (3 - 0 + 1) ) + 0;
         } else if (rarity == 'uncommon') {
-            let atk = Math.floor(Math.random() * (6 - 4 + 1) ) + 4;
-            let def = Math.floor(Math.random() * (6 - 4 + 1) ) + 4;
-            let defcha = Math.floor(Math.random() * (6 - 4 + 1) ) + 4;
-            let critcha = Math.floor(Math.random() * (6 - 4 + 1) ) + 4;
+            atk = Math.floor(Math.random() * (6 - 4 + 1) ) + 4;
+            def = Math.floor(Math.random() * (6 - 4 + 1) ) + 4;
+            defcha = Math.floor(Math.random() * (6 - 4 + 1) ) + 4;
+            critcha = Math.floor(Math.random() * (6 - 4 + 1) ) + 4;
         } else if (rarity == 'rare') {
-            let atk = Math.floor(Math.random() * (9 - 7 + 1) ) + 7;
-            let def = Math.floor(Math.random() * (9 - 7 + 1) ) + 7;
-            let defcha = Math.floor(Math.random() * (8 - 7 + 1) ) + 7;
-            let critcha = Math.floor(Math.random() * (8 - 7 + 1) ) + 7;
+            atk = Math.floor(Math.random() * (9 - 7 + 1) ) + 7;
+            def = Math.floor(Math.random() * (9 - 7 + 1) ) + 7;
+            defcha = Math.floor(Math.random() * (8 - 7 + 1) ) + 7;
+            critcha = Math.floor(Math.random() * (8 - 7 + 1) ) + 7;
         } else if (rarity == 'epic') {
-            let atk = Math.floor(Math.random() * (12 - 10 + 1) ) + 10;
-            let def = Math.floor(Math.random() * (12 - 10 + 1) ) + 10;
-            let defcha = Math.floor(Math.random() * (10 - 9 + 1) ) + 9;
-            let critcha = Math.floor(Math.random() * (10 - 9 + 1) ) + 9;
+            atk = Math.floor(Math.random() * (12 - 10 + 1) ) + 10;
+            def = Math.floor(Math.random() * (12 - 10 + 1) ) + 10;
+            defcha = Math.floor(Math.random() * (10 - 9 + 1) ) + 9;
+            critcha = Math.floor(Math.random() * (10 - 9 + 1) ) + 9;
         } else if (rarity == 'legendary') {
-            let atk = Math.floor(Math.random() * (16 - 13 + 1) ) + 13;
-            let def = Math.floor(Math.random() * (16 - 13 + 1) ) + 13;
-            let defcha = Math.floor(Math.random() * (12 - 11 + 1) ) + 11;
-            let critcha = Math.floor(Math.random() * (12 - 11 + 1) ) + 11;
+            atk = Math.floor(Math.random() * (16 - 13 + 1) ) + 13;
+            def = Math.floor(Math.random() * (16 - 13 + 1) ) + 13;
+            defcha = Math.floor(Math.random() * (12 - 11 + 1) ) + 11;
+            critcha = Math.floor(Math.random() * (12 - 11 + 1) ) + 11;
         }
 
             let element = "<card class='" + rarity + "' onclick='classList.add(\'flipped\')'><div class='cardfront'></div><div class='cardback'><span id='name'>" + name + "</span><br><span id='type' style='color: fuchsia; font-size: small;'>" + type + "</span><br><span><i style='color: greenyellow; padding-right: 10px;' class='fa-sharp fa-solid fa-plus'></i><span id='hp' tooltip='The health of this card. If it reaches 0, the card is removed permanently.'>" + hp + "</span></span><br><span><i style='color: red; padding-right: 10px;' class='fa-sharp fa-solid fa-hand-fist'></i><span id='atk' tooltip='The base attack of this card. This card will deal more damage the higher this value is.'>" + atk + "</span></span><br><span><i style='color: cornflowerblue; padding-right: 10px;' class='fa-sharp fa-solid fa-shield'></i><span id='def' tooltip='The base defense of this card. This card will block more damage the higher this value is.'>" + def + "%</span></span><br><span><i style='color: red; padding-right: 10px;' class='fa-sharp fa-solid fa-percent'></i><span id='critcha' tooltip='The crit chance of this card. Will be more likely to crit if this value is higher.'>" + critcha + "/24</span></span><br><span><i style='color: cornflowerblue; padding-right: 10px;' class='fa-sharp fa-solid fa-percent'></i><span id='defcha' tooltip='The defense chance of this card. Will be more likely to block if this value is higher.'>" + defcha + "/24</span></span></div></card>";
